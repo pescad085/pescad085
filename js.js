822db71003e8375a1dd464a47b74cf072e358730
pescad085 <84593475+pescad085@users.noreply.github.com>
2022-02-16 21:07:30 -0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+async function getBoredAPI() {
+    var apiURL = "https://www.boredapi.com/api/activity/";
+     var response = await fetch(apiURL);
+     //convert data to json
+     var data = await response.json();
+     // store data in different vars
+     
+     var {activity, participants } = data;
+           document.querySelector(".asideP").innerHTML = activity + ".";
+    }
+     //call function  
+     setInterval(getBoredAPI, 5000);
+
+
+    
+       
+
+
