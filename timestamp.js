3f1a746e98ee37e13b3f3cdafaf2c744cff3054d
pescad085 <jackbahjat@gmail.com>
2025-02-19 12:22:28 -0800

    everything updated

--- timestamp.js
+++ timestamp.js
@@ -4,3 +4,16 @@ let timeParagraph = document.getElementById("copyr");
 let date = new Date()
 const year = date.getFullYear();
 timeParagraph.innerHTML = year;
+
+
+
+
+// navigation bar js code 
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+mobileMenu.addEventListener('click', ()=> { 
+  navLinks.classList.toggle('active');
+
+});
+
+// End Of navigation bar code 
